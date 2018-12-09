306743d6333ea76eda6eceeafe1c0aaf9c5578cc
Kamil Czarnecki <kamoyl@outlook.com>
2018-12-09 14:25:26 +0100

    Added config file to store global variables

--- template.py
+++ template.py
@@ -6,15 +6,14 @@ from colorama import init,Fore, Back, Style
 init(autoreset=True)
 from termcolor import colored
 
-wine = print('\x1b[38;2;191;000;000m')
-lime = print('\x1b[38;2;191;255;000m')
+import config
 
 print(Fore.RED + 'some red text')
 print('automatically back to default color again')
 
-print('\033[31m' + 'some red text')
-print('\033[30m') # and reset to default color
-print('\x1b[38;2;191;255;000m' + 'test')
-
+print(config.wine + 'some red text')
+#print('\033[30m') # and reset to default color
+print(config.lime + 'test')
+print ('test')
 print(colored('Hello, World!', 'green', 'on_red'))
 
