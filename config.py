c496a070f22fbadc452f58b47da243188ada9db9
Kamil Czarnecki <kamoyl@outlook.com>
2018-12-19 11:08:32 +0100

    tdwallet finally tested, and works, no password needed then
    added some cleaning of constants values and migrating them to config

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python2.7
 
 import os
+import sys
 import datetime
+import logging
+import coloredlogs
 import socket
 from netaddr import IPNetwork, IPAddress
+import getopt
 
 wine = '\x1b[38;2;191;000;000m'
 limon = '\x1b[38;2;191;255;000m'
@@ -34,11 +38,18 @@ current_timestamp = datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
 os.environ["CURRENT_TIMESTAMP"] = current_timestamp
 office_network = '172.16.0.0/12'
 vpn_network = '10.65.64.0/18 129.189.0.0/16'
+dbalog = "MONITORING"
+dbalogv = "MONITORINGV"
+os.environ["DBALOG"] = dbalog
+os.environ["DBALOGV"] = dbalogv
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG',milliseconds=True)
 
 def usage():
-  print(lime + '   -o' + blue + '   directory to which will go all LOGs and TMPs')
-  print(blue + '        default: ' + os.environ['HOME'] + '/var')
-  print(lime + '   -v' + blue + '   verbose')
+  print(lime + '   -o, --output=' + blue + '   directory to which will go all LOGs and TMPs')
+  print(blue + '                   default: ' + cyan + os.environ['HOME'] + '/var')
+  print(lime + '   -v, --verbose' + blue + '   verbose')
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
